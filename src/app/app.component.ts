115ecac0a6e67ac1239d6f7f5083e77bf8551557
krk chaitanya <krkchaitanya42@gmail.com>
2018-03-15 12:00:21 -0500

    BrowserAnimationModuleInject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component ,trigger,state,style} from '@angular/core';
+//import {state,style} from "@angular/animations";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations:[
+    trigger('divState',[
+      state('normal',style({
+        backgroundColor:'red',
+        transform:'translateX(0)'
+      })),
+      state('highlighted',style({
+        backgroundColor:'lightblue',
+        transform:'translateX(100px)'
+      }))
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'app';
+  state='normal';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {FormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import {HttpModule, Http} from "@angular/http";
+
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations"
+
 
 
 @NgModule({
@@ -10,7 +16,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    HttpModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
